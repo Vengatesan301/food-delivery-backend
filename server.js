586e1fe833ac2a645dee9c93ce0a17b267d6a3b7
Vengatesan301 <kit.24.20bcs057@gmail.com>
2024-12-05 19:42:47 +0530

    changes

--- server.js
+++ server.js
@@ -36,6 +36,34 @@ app.get("/api/restaurants", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch data from Swiggy API" });
   }
 });
+console.log(":::::::::")
+app.get("/api/menu/:resId", async (req, res) => {
+  console.log("Request received at /api/menu/:resId");
+console.log("Received resId:", req.params.resId);
+
+  
+  const { resId } = req.params; // Get restaurantId from query params
+  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=${resId}`;
+
+  try {
+    //const response = await fetch(url);
+    const response = await fetch(url, { 
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Swiggy API error: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data,"data")
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data from Swiggy" });
+  }
+});
 
 // Start server
 app.listen(PORT, () => {
